bce06d0d614d38b6b3a1cca5c7ee4bc26749e6d4
remyryr <54060824+remyryr@users.noreply.github.com>
2025-12-06 14:50:24 +0100

    First commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import {Topic} from './topic/topic';
+import {Home} from './home/home';
+import {loginGuardGuard} from './login-guard-guard';
+import {Login} from './login/login';
+import {User} from './user/user';
+import {Topics} from './topics/topics';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', component: Home, canActivate: [loginGuardGuard]},
+  {path : 'topic/:id', component: Topic, canActivate: [loginGuardGuard]},
+  {path: 'login', component: Login},
+  {path: 'user', component: User, canActivate: [loginGuardGuard]},
+  {path : 'topics/:id', component: Topics}
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,23 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, inject, OnChanges, OnInit, signal, SimpleChanges} from '@angular/core';
+import {NavigationEnd, Router, RouterOutlet} from '@angular/router';
+import {filter} from 'rxjs';
+import {routes} from './app.routes';
+import {Header} from './header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header],
   templateUrl: './app.html',
+  standalone: true,
   styleUrl: './app.css'
 })
-export class App {
+export class App implements OnInit {
   protected readonly title = signal('forum-ph03');
+  router = inject(Router)
+  route = '';
+  ngOnInit() {
+    this.router.events.pipe(filter(event => event instanceof NavigationEnd)).subscribe((event: NavigationEnd) => {
+      this.route = event.url;
+    })
+  }
 }
